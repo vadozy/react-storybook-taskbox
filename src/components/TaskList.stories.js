c0a4297b7bfd6ba42a23cdb569a3c2f7d589fda6
Vadim Storozhuk <vadozy@yahoo.com>
2020-12-25 19:13:50 -0500

    Data

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import TaskList from "./TaskList";
+import { PureTaskList } from "./TaskList";
 import * as TaskStories from "./Task.stories";
 
 const taskListStories = {
-  component: TaskList,
+  component: PureTaskList,
   title: "TaskList",
   decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
 };
 export default taskListStories;
 
-const Template = (args) => <TaskList {...args} />;
+const Template = (args) => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
